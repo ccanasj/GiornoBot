a48eb44af09de02bc16fa37943c7ec8f86bbedf2
Catmizxc <casmis123@gmail.com>
2021-01-27 19:36:34 -0500

    Areglos a Zahando :weno:

--- index1.py
+++ index1.py
@@ -316,25 +316,24 @@ async def Sticky(ctx):
 '''
 
 @bot.command(aliases=['ZH'])
-async def Zahando(ctx,opcion, *, member: discord.Member = None):
-    async with ctx.channel.typing():
-        if not member:
-            member = ctx.message.author
-        avatar = member.avatar_url_as(format = 'png')
-        if opcion == 1:
-            await ctx.send("https://some-random-api.ml/canvas/gay?avatar=" + str(avatar))
-        elif opcion == 2:
-            await ctx.send("https://some-random-api.ml/canvas/triggered?avatar=" + str(avatar))
-        elif opcion == 3:
-            await ctx.send("https://some-random-api.ml/canvas/invert?avatar=" + str(avatar))
-        elif opcion == 4:
-            await ctx.send("https://some-random-api.ml/canvas/brightness?avatar=" + str(avatar))
-        elif opcion == 5:
-            await ctx.send("https://some-random-api.ml/canvas/blur?avatar=" + str(avatar))
-        elif opcion == 6:
-            await ctx.send("https://some-random-api.ml/canvas/glass?avatar=" + str(avatar))
-        elif opcion == 7:
-            await ctx.send("https://some-random-api.ml/canvas/wasted?avatar=" + str(avatar))
+async def Zahando(ctx,opcion:int = 1, *, member: discord.Member = None):
+    if not member:
+        member = ctx.message.author
+    avatar = member.avatar_url_as(format = 'png')
+    if opcion == 1:
+        await ctx.send("https://some-random-api.ml/canvas/gay?avatar=" + str(avatar))
+    elif opcion == 2:
+        await ctx.send("https://some-random-api.ml/canvas/triggered?avatar=" + str(avatar))
+    elif opcion == 3:
+        await ctx.send("https://some-random-api.ml/canvas/invert?avatar=" + str(avatar))
+    elif opcion == 4:
+        await ctx.send("https://some-random-api.ml/canvas/brightness?avatar=" + str(avatar))
+    elif opcion == 5:
+        await ctx.send("https://some-random-api.ml/canvas/blur?avatar=" + str(avatar))
+    elif opcion == 6:
+        await ctx.send("https://some-random-api.ml/canvas/glass?avatar=" + str(avatar))
+    elif opcion == 7:
+        await ctx.send("https://some-random-api.ml/canvas/wasted?avatar=" + str(avatar))
 '''
 @bot.listen()
 async def on_message_delete(message):
